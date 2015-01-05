47092a148ddb6237123316d51e904f77dccb9486
slavic.ronenko <slavic.ronenko@gmail.com>
2015-01-05 16:22:29 +0200

    Canvas context ready.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
-/**
- * Created by Ronenko on 05.01.2015.
- */
+'use strict';
+
+$(document).ready(function(){
+    var canvas,
+        workspace;
+
+    function init(id){
+        if(typeof id === 'string'){
+            canvas = document.getElementById(id);
+
+            if(!!canvas){
+                canvas.width = window.innerWidth;
+                canvas.height = window.innerHeight;
+
+                workspace = canvas.getContext('2d');
+            }
+        }
+    }
+
+    init('workspace');
+});
