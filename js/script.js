2085a4005c7275a685e02605be220536aba9e987
slavic.ronenko <slavic.ronenko@gmail.com>
2015-01-06 17:22:33 +0200

    checkSettings function added!

--- js/script.js
+++ js/script.js
@@ -1,29 +1,57 @@
-'use strict';
+(function () {
+    //'use strict';
 
-function CanvasSandbox(){
-    this.element;
-    this.context;
+    function CanvasSandbox() {
+        this.init = function (id) {
+            if (typeof id === 'string') {
+                this.element = document.getElementById(id);
 
-    this.init = function (id){
-        if(typeof id === 'string'){
-            this.element = document.getElementById(id);
+                if (!!this.element) {
+                    this.element.width = window.innerWidth;
+                    this.element.height = window.innerHeight;
 
-            if(!!this.element){
-                this.element.width = window.innerWidth;
-                this.element.height = window.innerHeight;
-
-                this.context = this.element.getContext('2d');
+                    this.context = this.element.getContext('2d');
+                }
             }
+
+            return this;
+        };
+
+        function checkSettings(settings, properties) {
+            var i = 0,
+                l = properties.length,
+                result = true;
+
+            for (i; i < l; i += 1) {
+                if (!settings.hasOwnProperty(properties[i])) {
+                    result = false;
+                }
+            }
+
+            return result;
         }
 
-        return this;
-    };
+        this.drawVector = function (settings) {
+            if (checkSettings(settings, ['x', 'y', 'angle', 'width'])) {
+                return this;
+            }
 
-    this.draw = function () {
+            return false;
+        };
 
+        this.draw = function () {
+            this.drawVector({
+                x: 228,
+                y: 322,
+                angle: 15,
+                width: 1488
+            });
+
+            return this;
+        };
     }
-}
 
-$(document).ready(function(){
-    (new CanvasSandbox()).init('workspace').draw();
-});
+    $(document).ready(function () {
+        (new CanvasSandbox()).init('workspace').draw();
+    });
+}());
