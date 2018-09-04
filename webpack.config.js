9694a66fd764e54876c007cca3e26a7e945b8fa6
viacheslav.ronenko <slavic.ronenko@gmail.com>
2018-09-04 23:28:45 +0300

    Add webpack, ts and stuff

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './app/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        enforce: 'pre',
+        loader: 'tslint-loader'
+      },
+      {test: /\.ts$/, loader: 'ts-loader'}
+    ]
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    hot: true,
+    open: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: './app/index.html'
+    })
+  ]
+};
