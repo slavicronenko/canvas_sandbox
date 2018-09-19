7f75b406e2c8c55321020e41b118c114585b1934
viacheslav.ronenko <slavic.ronenko@gmail.com>
2018-09-19 09:24:45 +0300

    Add some working code

--- app/index.ts
+++ app/index.ts
@@ -1,21 +1,47 @@
-import { Subject, timer } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { interval, merge, of, timer } from 'rxjs';
+import {
+  bufferCount, bufferTime, concatMap,
+  count,
+  debounce,
+  debounceTime,
+  delay,
+  distinct,
+  elementAt,
+  filter,
+  last,
+  map, max, min, reduce,
+  take,
+  takeLast
+} from 'rxjs/operators';
 
-const observer = {
-  next: (value) => {
-    console.log(`next: ${value}`);
-  },
-  complete: () => {
-    console.log(`complete!`);
-  }
-};
+function getObserver(name: string) {
+  return {
+    next: (value) => {
+      console.log(`${name} - next: ${value}`);
+    },
+    complete: () => {
+      console.log(`${name} - complete!`);
+    }
+  };
+}
 
-const observable = timer(0, 1000)
+const observable1 = interval(1278)
   .pipe(
+    take(5),
     map((x) => x * 2)
   );
 
-const subj = new Subject();
+const observable2 = interval(789)
+  .pipe(
+    take(5),
+    map((x) => x * 3)
+  );
 
-subj.subscribe(observer);
-observable.subscribe(subj);
+/*merge(observable1, observable2)
+  .pipe(
+    bufferTime(1000)
+  )
+  .subscribe(getObserver('obs1'));*/
+
+observable1.subscribe(getObserver('test1'));
+observable1.subscribe(getObserver('test2'));
