5761a76def1cc34efb1617934f797e7b9988970d
viacheslav.ronenko <slavic.ronenko@gmail.com>
2018-12-03 16:26:21 +0200

    Add utility method for calculating next point between two points

--- app/util.ts
+++ app/util.ts
@@ -14,3 +14,23 @@ export function generateString(size: number = 6, chars: string = 'abcdefghijklmn
 export function getDistanceBetween(a: ICoordinates, b: ICoordinates) {
   return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
 }
+
+export function getNextPosition(
+  current: ICoordinates,
+  target: ICoordinates,
+  speed: number,
+  lastPositionUpdate: number
+): ICoordinates {
+  const timePassed = (Date.now() - lastPositionUpdate) / 1000;
+  const timeToArrive = getDistanceBetween(current, target) / speed;
+  const difference = { x: target.x - current.x, y: target.y - current.y };
+  const distance = {
+    x: (difference.x / timeToArrive) * timePassed,
+    y: (difference.y / timeToArrive) * timePassed
+  };
+
+  return {
+    x: distance.x < Math.abs(difference.x) ? current.x + distance.x : target.x,
+    y: distance.y < Math.abs(difference.y) ? current.y + distance.y : target.y
+  };
+}
