82575689b0af166dd71795bb05bdac6a010ef2e9
viacheslav.ronenko <slavic.ronenko@gmail.com>
2018-12-07 00:25:05 +0200

    Add Target class

---
+++ app/Target.ts
@@ -0,0 +1,68 @@
+import { Point } from './Point';
+import { ITrackable } from './interfaces';
+
+export class Target {
+  constructor(tracked: ITrackable) {
+    this.tracked = tracked;
+    this.update();
+  }
+
+  private readonly tracked: ITrackable;
+  private readonly trace: ITraceData[] = [];
+
+  public update(): void {
+    const position = this.tracked.getCurrentPosition();
+
+    if (position) {
+      const time = Date.now();
+      let speed = null;
+      let acceleration = null;
+
+      if (this.trace.length) {
+        const {
+          position: lastPosition,
+          time: lastUpdate,
+          speed: lastSpeed
+        } = this.trace[this.trace.length - 1];
+        const secondsPassed = (1000 / (time - lastUpdate));
+
+        if (lastPosition) {
+          speed = lastPosition.distanceTo(position) * secondsPassed;
+
+          if (lastSpeed) {
+            acceleration = (speed - lastSpeed) * secondsPassed;
+          }
+        }
+      }
+
+      this.trace.push({ time, position, speed, acceleration });
+
+      if (this.trace.length > Target.MAX_TRACE_LENGTH) {
+        this.trace.shift();
+      }
+    }
+  }
+
+  public get position(): Point | null {
+    return this.trace.length ? this.trace[this.trace.length - 1].position : null;
+  }
+
+  public get speed(): number | null {
+    return this.trace.length ? this.trace[this.trace.length - 1].speed : null;
+  }
+
+  public get acceleration(): number | null {
+    return this.trace.length ? this.trace[this.trace.length - 1].acceleration : null;
+  }
+
+  private static get MAX_TRACE_LENGTH() {
+    return 10;
+  }
+}
+
+export interface ITraceData {
+  time: number;
+  position: Point;
+  speed: number;
+  acceleration: number;
+}
